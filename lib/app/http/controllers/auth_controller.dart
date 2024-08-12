deb6bdf9cc6acbdf23695654a1ad99656416e514
Daniel Marciano <danielmarciano.ti@belenus.com.br>
2024-08-12 14:11:02 -0300

    Fix: Auth validation

--- lib/app/http/controllers/auth_controller.dart
+++ lib/app/http/controllers/auth_controller.dart
@@ -7,7 +7,6 @@ import '../../data/repositories/user_repository.dart';
 import '../../models/error_model.dart';
 import '../../models/success_model.dart';
 import '../../utils/enums/http_error_enum.dart';
-import '../../utils/extensions/string_extensions.dart';
 import '../../utils/fp/either.dart';
 import '../../utils/mixins/password_hash_mixin.dart';
 import 'base_controller.dart';
@@ -18,19 +17,41 @@ class AuthController extends BaseController with PasswordHashMixin {
 
   AuthController(this._userRepository, this._authRepository);
 
+  Response? _validateAuthBody(Request req) {
+    try {
+      req.validate({
+        'email': 'required|email',
+        'password': 'required|min_length:6',
+      }, {
+        'email.required': 'O campo email é obrigatório',
+        'email.email': 'O campo email deve ser um email válido',
+        'password.required': 'O campo senha é obrigatório',
+        'password.min_length': 'O campo senha deve ter no mínimo 6 caracteres',
+      });
+      return null;
+    } catch (e) {
+      final errorMessage =
+          ((e as dynamic).message as Map<String, dynamic>).entries.first.value;
+      return Response.json(
+        ErrorModel(
+          message: errorMessage,
+          type: HttpErrorEnum.unprocessableEntity,
+        ).toMap(),
+        HttpStatus.unprocessableEntity,
+      );
+    }
+  }
+
   Future<Response> auth(Request req) async {
+    final errorResponse = _validateAuthBody(req);
+
+    if (errorResponse != null) {
+      return errorResponse;
+    }
+
     final body = req.body;
     final email = body['email'] as String?;
     final password = body['password'] as String?;
-
-    if (email.isNullOrEmpty || password.isNullOrEmpty) {
-      return Response.json(
-          ErrorModel(
-                  type: HttpErrorEnum.badRequest,
-                  message: 'Email e senha são obrigatórios')
-              .toMap(),
-          HttpStatus.badRequest);
-    }
 
     final user = await _userRepository.getUserByEmail(email!);
 
